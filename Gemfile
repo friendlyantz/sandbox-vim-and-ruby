1208486ad1bcc5ee979afcae219b3b862aa8ab15
Anton Panteleev <friendlyantz@pm.me>
2022-12-19 10:43:33 +1100

    Cleanup files for neoviming

--- Gemfile
+++
@@ -1,5 +0,0 @@
-source "https://rubygems.org"
-
-gem 'ice_nine'
-gem 'ruby-progressbar'
-gem 'pry'
--- progress_bar.rb
+++
@@ -1,8 +0,0 @@
-require 'ruby-progressbar'
-
-progress = ProgressBar.create
-100.times do
-  sleep 0.2
-  # puts 'Hey'
-  progress.increment
-end
